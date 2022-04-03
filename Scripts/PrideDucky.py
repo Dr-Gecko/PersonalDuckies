f34c4997806bbcdc9d4c5b0eddfeb38029686b77
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-04-03 12:04:09 -0700

    Update PrideDucky.py

--- Scripts/PrideDucky.py
+++ Scripts/PrideDucky.py
@@ -22,7 +22,7 @@ payload_delivered = 0
 time.sleep(2)
 print("Ready.")
 pause = 0.25
-def launch_terminal():
+def notepad():
     if operating_system is 1:
         led.value = False
         kbd.press(Keycode.GUI)
@@ -33,23 +33,29 @@ def launch_terminal():
         kbd.press(Keycode.ENTER)
         time.sleep(pause)
         kbd.release_all()
-        layout.write(" #        #####  ######  #######  #####        \n")
-        layout.write(" #       #     # #     #    #    #     #   #   \n")
-        layout.write(" #       #       #     #    #    #     #   #   \n")
-        layout.write(" #       #  #### ######     #    #     # ##### \n")
-        layout.write(" #       #     # #     #    #    #   # #   #   \n")
-        layout.write(" #       #     # #     #    #    #    #    #   \n")
-        layout.write(" #######  #####  ######     #     #### #       \n")
+        layout.write("  _     ____ ____ _____ ___ \n")
+        layout.write(" | |   / ___| __ )_   _/ _ \   _ \n")
+        layout.write(" | |  | |  _|  _ \ | || | | |_| |_ \n")
+        layout.write(" | |__| |_| | |_) || || |_| |_   _| \n")
+        layout.write(" |_____\____|____/ |_| \__\_\ |_| \n")
         layout.write("\n")
-        layout.write(" ######  ###  #####  #     # #######  #####    \n")
-        layout.write(" #     #  #  #     # #     #    #    #     #   \n")
-        layout.write(" #     #  #  #       #     #    #    #         \n")
-        layout.write(" ######   #  #  #### #######    #     #####    \n")
-        layout.write(" #   #    #  #     # #     #    #          #   \n")
-        layout.write(" #    #   #  #     # #     #    #    #     #   \n")
-        layout.write(" #     # ###  #####  #     #    #     #####    \n")
-        layout.write("        Remember, its gay to be homophobic!    \n")
-        layout.write("               Check the system date!            ")
+        layout.write("  ____  ___ ____ _   _ _____ ____  _ \n")
+        layout.write(" |  _ \|_ _/ ___| | | |_   _/ ___|| | \n")
+        layout.write(" | |_) || | |  _| |_| | | | \___ \| | \n")
+        layout.write(" |  _ < | | |_| |  _  | | |  ___) |_| \n")
+        layout.write(" |_| \_\___\____|_| |_| |_| |____/(_) \n")
+        layout.write("\n")
+        layout.write("   ____ _               _     __   __ \n")
+        layout.write("  / ___| |__   ___  ___| | __ \ \ / /__  _   _ _ __ \n")
+        layout.write(" | |   | '_ \ / _ \/ __| |/ /  \ V / _ \| | | | '__| \n")
+        layout.write(" | |___| | | |  __/ (__|   <    | | (_) | |_| | |  _ \n")
+        layout.write("  \____|_| |_|\___|\___|_|\_\   |_|\___/ \__,_|_| ( ) \n")
+        layout.write("                                                  |/  \n")
+        layout.write("  ____        _         _ \n")
+        layout.write(" |  _ \  __ _| |_ ___  | |    _ __ ___   __ _  ___ \n")
+        layout.write(" | | | |/ _` | __/ _ \ | |   | '_ ` _ \ / _` |/ _ \ \n")
+        layout.write(" | |_| | (_| | ||  __/ | |___| | | | | | (_| | (_) | \n")
+        layout.write(" |____/ \__,_|\__\___| |_____|_| |_| |_|\__,_|\___/ \n")
         kbd.press(Keycode.ENTER), kbd.release_all()
 
 def locksystem():
@@ -62,15 +68,15 @@ def locksystem():
         kbd.press(Keycode.ENTER)
         kbd.release_all()
         time.sleep(0.5)
-        layout.write("Rundll32.exe user32.dll,LockWorkStation & date 6/28/1970 & exit")
+        layout.write("Rundll32.exe user32.dll,LockWorkStation & date 06/26/2015 & exit")
         kbd.press(Keycode.ENTER), kbd.release_all()
 
 while True:
-    if button.value is True and payload_delivered is 0:  # run it
-        print("Release the water fowl!")  # for debugging in screen or putty
+    if button.value is True and payload_delivered is 0:
+        print("Release the water fowl!") # A for debugging in screen or putty
         if payload is 0:
-            launch_terminal()
-            #locksystem()
+            notepad()
+            locksystem()
             payload_delivered = 1
     if payload_delivered is 1:
      sys.exit()
